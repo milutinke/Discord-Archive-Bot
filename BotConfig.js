8e57c2d8b7427d97c27353803ccdb741eef22785
Dusan Milutinovic <BGteam@live.com>
2020-06-20 22:12:48 +0200

    Update BotConfig.js

--- BotConfig.js
+++ BotConfig.js
@@ -1,16 +1,16 @@
 module.exports = {
-    bot_token: "NzIxMzExNjU5NzI1MTYwNDU4.XuSr5g.uPYQ58Ri6mhk9k_hmSaPwihJfyo",
-    bot_client_id: "721311659725160458",
+    bot_token: "",
+    bot_client_id: "",
     bot_prefix: '!',
 
     attachments_path: 'Attachments',
     debug: true,
 
     database: {
-        mongodb_user: 'archive_bot',
-        mongodb_password: 'NekaJakaSifra123',
-        mongodb_host: '185.101.95.36',
-        mongodb_port: '23017',
-        mongodb_base: 'discord'
+        mongodb_user: '',
+        mongodb_password: '',
+        mongodb_host: '',
+        mongodb_port: '',
+        mongodb_base: ''
     }
 };
