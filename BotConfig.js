d303901d65dd2996527647d6b6db12bdf7b9baab
Dusan Milutinovic <BGteam@live.com>
2020-10-30 18:05:28 +0100

    Added BotConfig.js back
    It was deleted on accident

---
+++ BotConfig.js
@@ -0,0 +1,16 @@
+module.exports = {
+    bot_token: "",
+    bot_client_id: "",
+    bot_prefix: '!',
+
+    attachments_path: 'Attachments',
+    debug: true,
+
+    database: {
+        mongodb_user: '',
+        mongodb_password: '',
+        mongodb_host: '',
+        mongodb_port: '',
+        mongodb_base: ''
+    }
+};
